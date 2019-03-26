3b2bd72c8335dbeb8c00c2303b0eb3e12252821f
kov-serg <kov_serg@freemail.ru>
2019-03-26 23:16:20 +0300

    fix path

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-!/bin/sh
+#!/bin/sh
 
 cp engine-master/build/bin/gost.so libgost.so
 gcc -o get-cpcert -Iengine-master get-cpcert.c \
   -Lopenssl-OpenSSL_1_1_1-stable -lssl -lcrypto \
-  -Lengine-master/build -lgost_core -L. -lgost \
+  -Lengine-master/build/bin -lgost_core -L. -lgost \
   -lpthread -ldl -Xlinker '-rpath=.'
+
