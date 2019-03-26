e4cfa7f94cfa683a36f86887ef158135552a043c
kov-serg <kov_serg@freemail.ru>
2019-03-26 22:59:35 +0300

    Update build.sh
    change dir

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 !/bin/sh
 
-cp ../engine-master/build/bin/gost.so libgost.so
-gcc -o get-cpcert -I../engine-master get-cpcert.c \
-  -L.../openssl-OpenSSL_1_1_1-stable -lssl -lcrypto \
-  -L../engine-master/build -lgost_core -L. -lgost \
+cp engine-master/build/bin/gost.so libgost.so
+gcc -o get-cpcert -Iengine-master get-cpcert.c \
+  -Lopenssl-OpenSSL_1_1_1-stable -lssl -lcrypto \
+  -Lengine-master/build -lgost_core -L. -lgost \
   -lpthread -ldl -Xlinker '-rpath=.'
